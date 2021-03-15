3dc8c71a7d1c09149f9c9cc4ea3db9f627889728
Marco De La Pierre <marco.delapierre@gmail.com>
2021-03-15 13:53:36 +0800

    test_gadi files

---
+++ test/gadi.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#PBS -N nxf-small
+#PBS -P <NCI project>
+#PBS -q normal
+#PBS -l ncpus=1
+#PBS -l mem=8GB
+#PBS -l walltime=10:00
+
+### probably not needed as this is the default behaviour in Gadi, let us see
+###PBS -l storage=scratch/<NCI project>
+
+#PBS -l wd
+#PBS -W umask=022
+
+
+# Ensure nextflow is in the PATH
+export PATH=$PATH:<path/to/nextflow>
+
+nextflow run main.nf \
+  --reads='reads_{1,2}.fq.gz' \
+  -profile test_gadi \
+  -name nxf-${PBS_JOBID}
